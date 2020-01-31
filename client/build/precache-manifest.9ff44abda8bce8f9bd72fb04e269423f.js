ca5931417cd3b51e5961fe3b183dc90ad7773d2a
ivers523 <ivers523@umn.edu>
2020-01-31 15:30:33 -0600

    new mern file

---
+++ client/build/precache-manifest.9ff44abda8bce8f9bd72fb04e269423f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccfba34e1b2f55163281",
+    "url": "/static/css/main.2f491b96.chunk.css"
+  },
+  {
+    "revision": "ccfba34e1b2f55163281",
+    "url": "/static/js/main.bdf6a15d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "baa47432e45492e49029",
+    "url": "/static/js/2.b06e6636.chunk.js"
+  },
+  {
+    "revision": "e98e5fb6607be3609bc2b2b56846812a",
+    "url": "/index.html"
+  }
+];
