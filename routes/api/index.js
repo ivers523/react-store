db0ab73d318044537897fb0a2ab3093a8462a098
ivers523 <ivers523@umn.edu>
2020-01-31 15:47:18 -0600

    working

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
-const bookRoutes = require("./books");
+const productRoutes = require("./products");
+const userRoutes = require("./users");
 
-// Book routes
-router.use("/books", bookRoutes);
+// Post routes
+router.use("/products", productRoutes);
+router.use("/users", userRoutes);
 
 module.exports = router;
